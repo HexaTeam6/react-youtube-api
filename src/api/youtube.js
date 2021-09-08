9a1bf7009623fe5e926f40b8fa5d025392911f01
HexaTeam6 <wahed.18051@mhs.its.ac.id>
2021-09-09 03:13:23 +0700

    menambahkan key youtube api

---
+++ src/api/youtube.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export default axios.create({
+    baseUrl: 'http://www.googlepapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: 'AIzaSyAK-R94k759fpLV__WQ1VweeJuCDz8sRiM'
+    }
+})
