1ce0d65a5f7ddcbbe89406c8c380f6148be9e562
HexaTeam6 <wahed.18051@mhs.its.ac.id>
2021-09-09 11:16:06 +0700

    menambahkan video list components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import youtube from './api/youtube';
 
-import { SearchBar, VideoDetail } from './components';
+import { SearchBar, VideoDetail, VideoList } from './components';
 
 class App extends React.Component {
     state = {
         videos: [],
         selectedVideo: null,
+    }
+
+    componentDidMount(){
+        this.handleSubmit('Javascript')
+    }
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video})
     }
 
     handleSubmit = async (searchTerm) => {
@@ -27,7 +35,7 @@ class App extends React.Component {
     }
 
     render() {
-        const { selectedVideo } = this.state;
+        const { selectedVideo, videos } = this.state;
 
         return(
             <Grid justifyContent="center" container spacing={10}>
@@ -40,7 +48,7 @@ class App extends React.Component {
                             <VideoDetail video={selectedVideo}/>
                         </Grid>
                         <Grid item xs={4}>
-                            { /* Video list */ }
+                            <VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
                         </Grid>
                     </Grid>
                 </Grid>
